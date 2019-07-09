958b0fa8ae75180d1cf46fee75edac18f5fb388c
David Rosson <david@rosson.com.au>
2019-07-09 19:10:04 +0200

    Add placeholder for server

---
+++ aeneas-server/server.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const cors = require('cors');
+const app = express();
+app.use(cors());
+
+const SERVER_PORT = 3030;
+
+app.get('/hello', (req, res) => {
+  return res.json({ message: 'Hello!' });
+});
+
+app.listen(SERVER_PORT);
