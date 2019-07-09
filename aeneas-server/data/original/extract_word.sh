0d0b32c173406fc33a4605dcc01f617268463615
David Rosson <david@rosson.com.au>
2019-07-09 23:30:31 +0200

    Save user fragments as files

---
+++ aeneas-server/data/original/extract_word.sh
@@ -0,0 +1,5 @@
+ffmpeg -i ursache.wav -ss 0.000 -to 0.300 word_1.mp3
+ffmpeg -i ursache.wav -ss 0.320 -to 1.260 word_2.mp3
+ffmpeg -i ursache.wav -ss 1.400 -to 1.720 word_3.mp3
+ffmpeg -i ursache.wav -ss 1.760 -to 2.060 word_4.mp3
+ffmpeg -i ursache.wav -ss 2.100 -to 2.760 word_5.mp3
--- aeneas-server/server.js
+++ aeneas-server/server.js
@@ -15,16 +15,17 @@ app.use('/static', express.static('data'));
 
 app.post('/upload', upload.single('audioBlob'), (req, res) => {
   const timestamp = req.file.originalname;
-  const audioFilePath = `${__dirname}/data/${timestamp}.wav`;
+  const basePath = `${__dirname}/data`
+  const audioFilePath = `${basePath}/${timestamp}.wav`;
 
   fs.writeFileSync(
     audioFilePath,
     Buffer.from(new Uint8Array(req.file.buffer))
   );
 
-  const transcriptPath = `${__dirname}/data/original/transcript.txt`;
-  const mp3Path = `${__dirname}/data/${timestamp}.mp3`
-  const jsonPath = `${__dirname}/data/${timestamp}.json`
+  const transcriptPath = `${basePath}/original/transcript.txt`;
+  const mp3Path = `${basePath}/${timestamp}.mp3`
+  const jsonPath = `${basePath}/${timestamp}.json`
 
   const cmdOpusToMp3 = `ffmpeg -loglevel panic -i ${audioFilePath} -ss 0.040 -af silenceremove=1:0:-48dB -vn -ar 44100 -ac 2 -b:a 128k ${mp3Path}`
   shell.exec(cmdOpusToMp3);
@@ -34,6 +35,15 @@ app.post('/upload', upload.single('audioBlob'), (req, res) => {
 
   const results = JSON.parse(fs.readFileSync(jsonPath));
   results.id = timestamp;
+
+  const fragments = results.fragments;
+  fragments.forEach((fragment, idx) => {
+    const fileSuffix = idx + 1;
+    const startTime = fragment.begin;
+    const endTime = fragment.end;
+    const cmdExtractSection = `ffmpeg -loglevel panic -i ${mp3Path} -ss ${startTime} -to ${endTime} -acodec copy ${basePath}/${timestamp}_${fileSuffix}.mp3 `;
+    shell.exec(cmdExtractSection);
+  })
 
   console.log(`Processed in ${Date.now()-Number(timestamp)} ms`)
   return res.json(results);
